030f4ca1d90d7ae3a5c03d26ddc535cfac61e0b4
Leon Nguyen <vicnguyenvuong@gmail.com>
2022-05-18 15:59:19 +0700

    fix bug config

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract QKCToken is ERC20, Ownable {
-//     event Claim(address owner, address indexed user, uint amount);
+contract QKCToken is ERC20, Ownable {
+    event Claim(address owner, address indexed user, uint amount);
 
-//     constructor() ERC20("EvonDev", "EVD") {
-//         _mint(msg.sender, 10000 * 10**18); //1000000000000000000
-//     }
+    constructor(uint totalSuply) ERC20("EvonDev", "EVD") {
+        _mint(msg.sender, totalSuply * 10**18); //1000000000000000000
+    }
 
-//     function claimToken(uint amount) public {
-//         address user = _msgSender();
-//         address owner = owner();
-//         _approve(owner, user, amount * 10**18);
-//         transferFrom(owner, user, amount * 10**18);
-//         emit Claim(owner, user, amount);
-//     }
-//}
-
-contract BEP20 is ERC20, Ownable {
-    constructor() ERC20("EvonDev", "EVD") {
-        _mint(msg.sender, 100 * 10**18);
+    function claimToken(uint amount) public {
+        address user = _msgSender();
+        address owner = owner();
+        _approve(owner, user, amount * 10**18);
+        transferFrom(owner, user, amount * 10**18);
+        emit Claim(owner, user, amount);
     }
 }
+
+// contract BEP20 is ERC20, Ownable {
+//     constructor() ERC20("EvonDev", "EVD") {
+//         _mint(msg.sender, 100 * 10**18);
+//     }
+// }
--- test/Token.js
+++ test/Token.js
@@ -1,46 +1,67 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token contract", function () {
+// describe("Token contract", function () {
+//   it("Deployment should assign the total supply of tokens to the owner", async function () {
+//     const [owner] = await ethers.getSigners();
+
+//     const Token = await ethers.getContractFactory("BEP20");
+
+//     const hardhatToken = await Token.deploy();
+
+//     const ownerBalance = await hardhatToken.balanceOf(owner.address);
+//     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+//   });
+// });
+const {
+  experimentalAddHardhatNetworkMessageTraceHook,
+} = require("hardhat/config");
+const {
+  isCallTrace,
+} = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+let hardhatToken;
+let owner;
+let addr1;
+let addr2;
+let addrs;
+let Token;
+beforeEach(async () => {
+  myToken = await ethers.getContractFactory("QKCToken");
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  hardhatToken = await myToken.deploy(1000);
+});
+describe("Deployment", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const Token = await ethers.getContractFactory("BEP20");
-
-    const hardhatToken = await Token.deploy();
-
-    // const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    // expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
 });
-// const {
-//   experimentalAddHardhatNetworkMessageTraceHook,
-// } = require("hardhat/config");
-// const {
-//   isCallTrace,
-// } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
-// let hardhatToken;
-// let owner;
-// let addr1;
-// let addr2;
-// let addrs;
-// let Token;
-// // beforeEach(async () => {
+describe("Transaction", async function () {
+  it("User 1 claim token", async function () {
+    await hardhatToken.connect(addr1).claimToken(100);
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(
+      ethers.BigNumber.from("100000000000000000000")
+    );
+  });
+  it("User 2 claim token", async function () {
+    await hardhatToken.connect(addr1).claimToken(200);
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(
+      ethers.BigNumber.from("200000000000000000000")
+    );
+  });
 
-// // });
-// describe("Deployment", function () {
-//   it("Deployment should assign the total supply of tokens to the owner", async function () {
-//     myToken = await ethers.getContractFactory("BEP20");
-//     hardhatToken = await myToken.deploy(100);
-//     // [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-//     // const ownerBalance = await hardhatToken.balanceOf(owner.address);
-//     // expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-//   });
-// });
-// describe("Transaction", async function () {
-//   it("User 1 claim token", async function () {
-//     await hardhatToken.connect(addr1).get(100);
-//     const addr1Balance = await hardhatToken.balanceOf(addr1.address);
-//     expect(addr1Balance).to.equal(100000000000000000000);
-//   });
-// });
+  it("User 1 & 2 claim token", async function () {
+    await hardhatToken.connect(addr1).claimToken(100);
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(
+      ethers.BigNumber.from("100000000000000000000")
+    );
+    await hardhatToken.connect(addr2).claimToken(200);
+    const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+    expect(addr2Balance).to.equal(
+      ethers.BigNumber.from("200000000000000000000")
+    );
+  });
+});
